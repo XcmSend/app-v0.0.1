2c6e6ed6c7722e1ef513fdc022343a6ef8dbcd4b
Filip <flipchan@riseup.net>
2023-10-03 00:46:47 +0200

    fixes websocket wss connection problems once deployed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import topLevelAwait from "vite-plugin-top-level-await";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,6 +11,14 @@ export default defineConfig({
       promiseImportName: i => `__tla_${i}`
     })  
   ],
+  server: {
+       host: true,
+	      port: 5173,
+	      strictPort: true,
+	  hmr: {
+      overlay: false, // Disable HMR overlay
+    },
+  },
   allowImportingTsExtensions: true,
   define: {
     // By default, Vite doesn't include shims for NodeJS/
